dff4c5ed1c8f3de2159a5984806dbab40c1e9afa
samikkk <osakwe0120@gmail.com>
2025-04-16 16:47:15 +0300

    For vh1dzdev

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 import uvicorn
 from loguru import logger
+from contextlib import asynccontextmanager
 
 from src.api.routes import router as api_router
 from fastapi.responses import RedirectResponse
 
-app = FastAPI()
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     await YOUR_FUNCTION()
+#     yield
+
+app = FastAPI() # В аргументы введи lifespan=lifespan и удали комм этот
 
 app.include_router(api_router)
 
